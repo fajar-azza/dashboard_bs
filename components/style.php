944916201636b870676cb52a5915766af338e0fd
agoengBani <agungsyabani99@gmail.com>
2025-01-10 06:23:27 +0700

    memperbaiki kesalahan

--- components/style.php
+++ components/style.php
@@ -3,13 +3,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Dashboard Admin</title>
   <!-- base:css -->
-  <link rel="stylesheet" href="assets/vendors/typicons/typicons.css">
-  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="../assets/vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="assets/images/favicon.ico" />
+  <link rel="shortcut icon" href="../assets/images/favicon.ico" />
   
--- components/top-nav.php
+++ components/top-nav.php
@@ -1,8 +1,8 @@
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-          <a class="navbar-brand brand-logo" href="index.html"><img src="assets/images/logo.svg" alt="logo"/></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo"/></a>
+          <a class="navbar-brand brand-logo" href="index.html"><img src="../assets/images/logo.svg" alt="logo"/></a>
+          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="../assets/images/logo-mini.svg" alt="logo"/></a>
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="typcn typcn-th-menu"></span>
           </button>
@@ -12,7 +12,7 @@
         <ul class="navbar-nav me-lg-2">
           <li class="nav-item nav-profile dropdown">
             <a class="nav-link" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-              <img src="assets/images/faces/face5.jpg" alt="profile"/>
+              <img src="../assets/images/faces/face5.jpg" alt="profile"/>
               <span class="nav-profile-name">Fajar Azza Najwa</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
@@ -20,7 +20,7 @@
                 <i class="typcn typcn-cog-outline text-primary"></i>
                 Settings
               </a>
-              <a class="dropdown-item" href="login/fungsi/logout.php">
+              <a class="dropdown-item" href="../login/fungsi/logout.php">
                 <i class="typcn typcn-eject text-primary"></i>
                 Logout
               </a>
@@ -46,7 +46,7 @@
               <p class="mb-0 fw-normal float-start dropdown-header">Messages</p>
               <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                    <img src="../../../assets/images/faces/face4.jpg" alt="image" class="profile-pic">
+                    <img src="../../../../assets/images/faces/face4.jpg" alt="image" class="profile-pic">
                 </div>
                 <div class="preview-item-content flex-grow">
                   <h6 class="preview-subject ellipsis fw-normal">David Grey
@@ -58,7 +58,7 @@
               </a>
               <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                    <img src="../../../assets/images/faces/face2.jpg" alt="image" class="profile-pic">
+                    <img src="../../../../assets/images/faces/face2.jpg" alt="image" class="profile-pic">
                 </div>
                 <div class="preview-item-content flex-grow">
                   <h6 class="preview-subject ellipsis fw-normal">Tim Cook
@@ -70,7 +70,7 @@
               </a>
               <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                    <img src="../../../assets/images/faces/face3.jpg" alt="image" class="profile-pic">
+                    <img src="../../../../assets/images/faces/face3.jpg" alt="image" class="profile-pic">
                 </div>
                 <div class="preview-item-content flex-grow">
                   <h6 class="preview-subject ellipsis fw-normal"> Johnson
