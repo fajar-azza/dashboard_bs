fbe2e2214b52144d87295da27aa758c9333d5b74
agoengBani <agungsyabani99@gmail.com>
2025-01-10 07:02:53 +0700

    membuat fungsi transaksi

--- dashboard/pages/fungsi_transaksi/t_peminjaman.php
+++ dashboard/pages/fungsi_transaksi/t_peminjaman.php
@@ -1,1 +1,55 @@
-helllooo
+<?php
+session_start();
+include('../../assets/koneksi.php');
+
+if (!isset($_POST['btn-submit'])) {
+    header('location: ../../?page=dashboard');
+    exit();
+}
+
+$nik_anggota = $_POST['nik_anggota'];
+$nama_anggota = $_POST['nama_anggota'];
+$tgl_pinjam = $_POST['tgl_pinjam'];
+
+$buku = [];
+for ($i = 5; $i <= 5; $i++) {
+    $kode_buku = $_POST["buku$i"] ? $_POST["buku$i"] : null;
+    $judul_buku = $_POST["judul$i"] ? $_POST["judul$i"] : null;
+    if (!empty($kode_buku)) {
+        $buku[] = ['kode_buku' => $kode_buku, 'judul_buku' => $judul_buku];
+    }
+}
+
+
+if ($nik_anggota == '') {
+    $_SESSION['msg']['err-nik'] = 'NIK tidak boleh kosong';
+}
+if ($tgl_pinjam == '') {
+    $_SESSION['msg']['err-tgl'] = 'Tentukan tanggal peminjaman';
+}
+if (empty($buku)) {
+    $_SESSION['msg']['err-buku'] = 'Pilih buku yang ingin dipinjam';
+} else {
+    $code = []; // Array untuk menyimpan kode buku yang diinput
+    foreach ($buku as $buk) {
+        // Validasi apakah buku dengan kode yang sama sudah ada di input
+        if (in_array($buk['code'], $code)) {
+            $_SESSION['msg']['err-buku'] = "Tidak bisa meminjam buku dengan kode yang sama!";
+            break;
+        }
+        $code[] = $buk['code']; // Tambahkan kode ke array jika belum ada
+
+        // Validasi apakah buku ada di database
+        $sql = "SELECT * FROM buku WHERE kode_b='{$buk['code']}'";
+        $query = mysqli_query($connect, $sql);
+        if (mysqli_num_rows($query) == 0) {
+            $_SESSION['msg']['buk'] = "Buku dengan kode '{$buk['code']}' tidak ditemukan!";
+            break;
+        }
+    }
+}
+
+if (isset($_SESSION['msg'])) {
+    header('location: ../../?page=t-pinjam');
+    exit();
+}
--- dashboard/pages/t-pinjam.php
+++ dashboard/pages/t-pinjam.php
@@ -14,8 +14,7 @@
                             <div class="form-group">
                                 <label for="exampleInputUsername1">NIK</label>
                                 <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search..." aria-label="search"
-                                        aria-describedby="search">
+                                    <input type="text" class="form-control" placeholder="Search..." name="nik_anggota">
                                     <div class="input-group-prepend d-flex">
                                         <span class="input-group-text" id="search">
                                             <button class="btn btn-dark btn-icon-text">
@@ -25,15 +24,26 @@
                                         </span>
                                     </div>
                                 </div>
+                                <?php 
+                                if(isset($_SESSION['msg']['err-nik'])){
+                                    echo '<span class="text-danger">'.$_SESSION['msg']['err-nik'].'</span>';
+                                }
+                                ?>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Nama</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Nama">
+                                <input readonly type="text" class="form-control" id="" placeholder="Nama"
+                                    name="nama_anggota">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Tgl Peminjaman</label>
-                                <input type="date" class="form-control" id="exampleInputPassword1" placeholder="">
+                                <input type="date" class="form-control" name="tgl_pinjam">
                             </div>
+                            <?php 
+                                if(isset($_SESSION['msg']['err-tgl'])){
+                                    echo '<span class="text-danger">'.$_SESSION['msg']['err-tgl'].'</span>';
+                                }
+                            ?>
                             <div class="text-end">
                                 <button type="submit" name="btn-submit" class="btn btn-primary me-2">Submit</button>
                             </div>
@@ -42,37 +52,37 @@
                 </div>
                 <div class="col-md-6">
                     <div class="card-body">
+                        <?php 
+                        if(isset($_SESSION['msg']['err-buku'])){
+                          echo '
+                              <div class="alert alert-danger" role="alert">
+                                  '.$_SESSION['msg']['err-buku'].'
+                              </div>
+                          ';
+                        }
+                        ?>
                         <div class="card-body">
+                            <?php 
+                            $jmlBuku = 5;
+                            for ($i = 1; $i <= $jmlBuku; $i++) {
+                            ?>
                             <p class="display-4">
-                                <u>Buku 1</u>
+                                <u>Buku <?= $i; ?></u>
                             </p>
                             <div class="form-group row">
-                                <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Kode</label>
+                                <label class="col-sm-3 col-form-label">Kode</label>
                                 <div class="col-sm-9">
-                                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="kode">
+                                    <input type="text" class="form-control" placeholder="kode" name="buku<?= $i; ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="exampleInputMobile" class="col-sm-3 col-form-label">Judul</label>
+                                <label class="col-sm-3 col-form-label">Judul</label>
                                 <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="exampleInputMobile" placeholder="judul">
+                                    <input readonly type="text" class="form-control" id="" placeholder="judul"
+                                        name="judul<?= $i; ?>">
                                 </div>
                             </div>
-                            <p class="display-4">
-                                <u>Buku 2</u>
-                            </p>
-                            <div class="form-group row">
-                                <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Kode</label>
-                                <div class="col-sm-9">
-                                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Kode">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="exampleInputMobile" class="col-sm-3 col-form-label">Judul</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="exampleInputMobile" placeholder="judul">
-                                </div>
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -80,3 +90,5 @@
         </form>
     </div>
 </div>
+
+<?php unset($_SESSION['msg']) ?>
